02b10711dd9aa5b72e0d82e1790ce421359b0de2
Zaymus <carson.brown47@gmail.com>
2021-08-17 21:30:23 -0400

    changed links to smooth scrolling

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,16 +1,30 @@
-const mission = document.getElementById("MS");
-const portfolio = document.getElementById("P");
-const team = document.getElementById("IT");
+const links = document.querySelectorAll("div.nav > p");
 
-mission.onclick = () => {
-    location.href = "#mission";
+for (const link of links) {
+    link.addEventListener("click", clickHandler);
 }
 
-portfolio.onclick = () => {
-    location.href = "#portfolio";
-    window.scrollTo(window.scrollX, window.scrollY + 70);
-}
+function clickHandler(e) {
+    e.preventDefault();
+    const id = this.getAttribute("id");
+    console.log(id);
+    console.log(id === "MS")
+    var offsetTop = 0;
+    if (id === "MS")
+    {
+        var offsetTop = document.getElementById("mission").offsetTop;
+    }
+    else if (id === "P")
+    {
+        var offsetTop = document.getElementById("portfolio").offsetTop;
+    }
+    else if (id === "IT")
+    {
+        var offsetTop = document.getElementById("team").offsetTop;
+    }
 
-team.onclick = () => {
-    location.href = "#team";
+    scroll({
+        top: offsetTop,
+        behavior: "smooth"
+    });
 }
